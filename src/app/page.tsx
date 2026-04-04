8129722e2df234e5c50c5040f6ff459f44bbe162
sidharth-spec <siddharthjaiswal717@gmail.com>
2026-04-04 22:21:08 +0530

    02:database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
-const Page = () => {
-  return <div>Hello world</div>;
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
+
+const Page = async () => {
+  const users = await prisma.user.findMany();
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(users)}
+    </div>
+  );
 };
 export default Page;
