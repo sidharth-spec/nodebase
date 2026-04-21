fc2c28f33eddc92668cc195588264bb9e6215809
sidharth-spec <siddharthjaiswal717@gmail.com>
2026-04-21 14:26:47 +0530

    22: google form trigger

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,5 +1,6 @@
 import { Connection, Node } from "@/generated/prisma/client";
 import toposort from "toposort";
+import { inngest } from "./client";
 export const topologicalSort = (
   nodes: Node[],
   connections: Connection[],
@@ -34,4 +35,14 @@ export const topologicalSort = (
   }
   const nodeMap = new Map(nodes.map((n) => [n.id, n]));
   return sortedNodeIds.map((id) => nodeMap.get(id)!).filter(Boolean);
+};
+
+export const sendWorkflowExecution = async (data: {
+  workflowId: string;
+  [key: string]: any;
+}) => {
+  return inngest.send({
+    name: "workflows/execute.workflow",
+    data,
+  });
 };
